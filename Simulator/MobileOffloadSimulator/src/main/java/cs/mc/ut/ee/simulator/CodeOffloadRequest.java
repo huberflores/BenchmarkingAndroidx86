034a63af2ddfb9c8c8886b9de4887fa2811127a6
Huber Flores <kyoraul@gmail.com>
2014-06-14 18:43:21 +0300

    load simulator added

---
+++ Simulator/MobileOffloadSimulator/src/main/java/cs/mc/ut/ee/simulator/CodeOffloadRequest.java
@@ -0,0 +1,22 @@
+package cs.mc.ut.ee.simulator;
+
+import cs.mc.ut.ee.algorithms.BubbleSort;
+
+
+/*
+ * author Huber Flores
+ */
+
+public class CodeOffloadRequest implements Runnable {
+	
+	BubbleSort client;
+
+	public void run() {
+		// TODO Auto-generated method stub
+		client = new BubbleSort(0);
+	}
+	
+
+	
+
+}
