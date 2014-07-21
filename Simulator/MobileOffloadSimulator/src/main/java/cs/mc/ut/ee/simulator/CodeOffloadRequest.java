e111973e43df967e625dd2a664f319d2feb77c20
Huber Flores <kyoraul@gmail.com>
2014-07-21 20:02:25 +0300

    use case added

--- Simulator/MobileOffloadSimulator/src/main/java/cs/mc/ut/ee/simulator/CodeOffloadRequest.java
+++ Simulator/MobileOffloadSimulator/src/main/java/cs/mc/ut/ee/simulator/CodeOffloadRequest.java
@@ -26,8 +26,10 @@ public class CodeOffloadRequest implements Runnable {
 		
 		client = new MiniMaxRemote();
 		
-		
-		int[][] board = {{-3, -5, -4, -2, -1, -4, -5, -3},
+		/**
+		 * Initial board
+		 */
+		/*int[][] board = {{-3, -5, -4, -2, -1, -4, -5, -3},
 				{-6, -6, -6, -6, -6, -6, -6, -6},
 				{0, 0, 0, 0, 0, 0, 0, 0},
 				{0, 0, 0, 0, 0, 0, 0, 0},
@@ -36,11 +38,27 @@ public class CodeOffloadRequest implements Runnable {
 				{6, 6, 6, 6, 6, 6, 6, 6},
 				{3, 5, 4, 2, 1, 4, 5, 3}
 				};
+		*/
+		
+		/**
+		 * Game in progress
+		 */
+		
+		int [][] board = {{0, -3, -4, -2, -1, -4, 0, -3}, 
+				{0, 0, -6, -6, -6, 0, -6, -6}, 
+				{-6, -6, 0, 0, 0, 0, 0, -5}, 
+				{-5, 0, 0, 5, 0, 0, 0, 0}, 
+				{0, 0, 6, 0, 6, -6, 0, 0}, 
+				{6, 6, 0, 6, 0, 6, 0, 6}, 
+				{0, 0, 0, 0, 0, 0, 6, 0}, 
+				{3, 0, 0, 2, 1, 4, 5, 3}
+				};
 		
 		int [] chess = new int[64];
 		for (int i=0;i<8;i++)
 			for (int j=0;j<8;j++)
 				chess[j*8+i]=board[i][j];
+		
 		
 		
 		float [] steps = client.minimax(chess, 4, false);
