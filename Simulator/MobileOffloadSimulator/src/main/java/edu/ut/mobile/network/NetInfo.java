ac7f3cab744405ba9a6c8a351e74761df7daf09b
Huber Flores <kyoraul@gmail.com>
2014-08-19 02:18:15 +0300

    change for tests

--- Simulator/MobileOffloadSimulator/src/main/java/edu/ut/mobile/network/NetInfo.java
+++ Simulator/MobileOffloadSimulator/src/main/java/edu/ut/mobile/network/NetInfo.java
@@ -1,9 +1,11 @@
 package edu.ut.mobile.network;
 
 public class NetInfo{
-	static byte[] IPAddress = {Integer.valueOf("192").byteValue(),Integer.valueOf("168").byteValue(),Integer.valueOf("43").byteValue(),Integer.valueOf("85").byteValue()};
+	
+	public static byte[] IPAddress = {Integer.valueOf("54").byteValue(),Integer.valueOf("73").byteValue(),Integer.valueOf("28").byteValue(),Integer.valueOf("236").byteValue()};
+	//static byte[] IPAddress = {Integer.valueOf("192").byteValue(),Integer.valueOf("168").byteValue(),Integer.valueOf("43").byteValue(),Integer.valueOf("85").byteValue()};
 	static int port = 6000;
-	static int waitTime = 10000;          
+	static int waitTime = 100000;          
 	  
 } 
 
